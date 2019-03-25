b9cac63ff12ac7115f529c604e9c36095a930669
Ryan David Forsyth <forsyth.r@gmail.com>
2019-03-24 21:31:05 -0400

    Create Searchbox, add font and styling

--- src/card.js
+++ src/card.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+//Destructuring the input so that the properties can be used directly
 const Card = ({ name, email, id }) => {
 
 	return(
 
 		<div className = 'tc bg-light-green dib br3 ps3 ma2 grow bw2 shadow-5'>
-			<img alt='Robot' src= {`https://robohash.org/${id}?200x200`} />
+			<img alt='Robot' src= {`http://robohash.org/${id}?set=set3`} />
 			<div>
 
 				<h2>{name}</h2>
